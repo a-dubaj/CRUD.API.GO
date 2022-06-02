2cab06f7d604a3242624710e50431856b4c8ac56
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-06-02 13:53:57 +0200

    Update

---
+++ main.go
@@ -0,0 +1,55 @@
+package CRUD_API_GO
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "438277",
+		Title: "Movie One",
+		Director: &Director{
+			Firstname: "John",
+			Lastname:  "Doe",
+		},
+	})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "7288828",
+		Title: "Movie Two",
+		Director: &Director{
+			Firstname: "Steve",
+			Lastname:  "Smith",
+		},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
+
+	fmt.Println("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
